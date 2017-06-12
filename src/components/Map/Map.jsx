08a0b3f9006f1dca1ee95f509a7ec8c9cb148093
Javier Enríquez Lávida <javier.lavida@gmail.com>
2017-06-12 17:50:19 -0500

    Initial commit

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div>
+        This will be a mapamundi
+      </div>
+    );
+  }
+}
+
+export default Map;
---
+++ src/routes/drivers/DriversContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DriverForm from '../../components/Driver/DriverForm';
+
+class DriversContainer extends React.Component {
+  render() {
+    return (
+      <DriverForm />
+    );
+  }
+}
+
+export default DriversContainer;
